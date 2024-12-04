5d1b6c1635017d1754a7d36b8069b0ed8ea0638e
Evelyn R <evelynrodriguezc0@gmail.com>
2024-12-04 17:49:47 -0500

    project structure

---
+++ backend/src/models/Recipe.ts
@@ -0,0 +1,19 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IRecipe extends Document {
+    title: string;
+    categrory: string;
+    ingredients: string[];
+    instructions: string;
+    image?: string;
+}
+
+const RecipeSchema: Schema = new Schema({
+    title: { type: String, required: true },
+    category: { type: String, required: true },
+    ingredients: { type: [String], required: true },
+    instructions: { type: String, required: true },
+    image: { type: String }
+});
+
+export default mongoose.model<IRecipe>('Recipe', RecipeSchema);
